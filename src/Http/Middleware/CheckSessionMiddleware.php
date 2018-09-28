66320f05545589d133c14a3cbbafcd6af421577e
rbayer <rbayer@sbts.edu>
2018-09-28 15:23:42 -0400

    remove echos

--- src/Http/Middleware/CheckSessionMiddleware.php
+++ src/Http/Middleware/CheckSessionMiddleware.php
@@ -9,9 +9,7 @@ class CheckSessionMiddleware
 
     public function handle($request, Closure $next)
     {
-        echo "in middleware\n";
         if (!$request->session()->isStarted()) {
-        echo "throwing exception\n";
             throw new \Exception('Sessions are required for CAS Server.  Please re-enable the session middleware.');
         }
 
